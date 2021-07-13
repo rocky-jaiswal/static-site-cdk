1b30c372be8e7cd220654ac4f99e0f99fdf87494
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2021-07-13 10:22:08 -0400

    fix script

--- app.py
+++ app.py
@@ -8,6 +8,20 @@ from aws_cdk import core as cdk
 from static_site_cdk.static_site_stack import StaticSiteStack
 
 
+def synth(domain, contents, account, region):
+    app = cdk.App()
+
+    StaticSiteStack(
+        app,
+        "StaticSiteStack",
+        env=cdk.Environment(account=account, region=region),
+        site_domain=domain,
+        site_contents=contents,
+    )
+
+    app.synth()
+
+
 parser = ArgumentParser()
 parser.add_argument("-d", "--domain", dest="domain", help="site domain")
 parser.add_argument("-c", "--contents", dest="contents", help="site contents")
@@ -15,22 +29,4 @@ parser.add_argument("-a", "--accountid", dest="account_id", help="AWS account id
 parser.add_argument("-r", "--region", dest="region", help="main AWS region")
 
 args = parser.parse_args()
-
-
-def synth():
-    SITE_DOMAIN = args.domain
-    SITE_CONTENTS = args.contents
-    ACCOUNT = args.content_id
-    REGION = args.region
-
-    app = cdk.App()
-
-    StaticSiteStack(
-        app,
-        "StaticSiteStack",
-        env=cdk.Environment(account=ACCOUNT, region=REGION),
-        site_domain=SITE_DOMAIN,
-        site_contents=SITE_CONTENTS,
-    )
-
-    app.synth()
+synth(args.domain, args.contents, args.account_id, args.region)
